c70f7df0255ab5a000e4e7333848d9e5ae470b1c
Heorhi Heilik <heheilik@icloud.com>
2023-10-25 10:40:12 +0300

    Empty project created

---
+++ RSSReader/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  RSSReader
+//
+//  Created by Heorhi Heilik on 25.10.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        let window = UIWindow()
+        window.rootViewController = ViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ RSSReader/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by Heorhi Heilik on 25.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
+
