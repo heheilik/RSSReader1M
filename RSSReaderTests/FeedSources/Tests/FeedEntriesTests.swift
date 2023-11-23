569bbda2ac637c30e2e4d1ea977fd50f531e2f70
Heorhi Heilik <heheilik@icloud.com>
2023-11-23 17:12:27 +0300

    Added setUp and tearDown for FeedEntriesTests

--- RSSReaderTests/FeedSources/Tests/FeedEntriesTests.swift
+++ RSSReaderTests/FeedSources/Tests/FeedEntriesTests.swift
@@ -5,9 +5,32 @@
 //  Created by Heorhi Heilik on 23.11.23.
 //
 
-import Foundation
+import FMArchitecture
 import XCTest
 
 class FeedEntriesTests: XCTestCase {
+
+    var viewModel: FeedEntriesViewModel?
+
+    // MARK: Lifecycle
+
+    override func setUp() {
+        guard let mockRSSFeed = MockFeeds.mockRSS.feed?.rssFeed else {
+            fatalError("Can't get feed from MockFeeds enum.")
+        }
+        viewModel = FeedEntriesViewModel(
+            dataSource: FMTableViewDataSource(tableView: nil),
+            context: FeedEntriesContext(
+                feedName: "Test",
+                rssFeed: mockRSSFeed
+            )
+        )
+    }
+
+    override func tearDown() {
+        viewModel = nil
+    }
+
+    // MARK: Tests
 
 }
