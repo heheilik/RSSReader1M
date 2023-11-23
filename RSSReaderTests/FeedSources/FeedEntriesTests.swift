d2774eb1d7699c0757118e83486bc86eae2c9ba3
Heorhi Heilik <heheilik@icloud.com>
2023-11-23 16:24:56 +0300

    Added FeedEntriesTests class + mock RSS feed

---
+++ RSSReaderTests/FeedSources/FeedEntriesTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedEntriesTests.swift
+//  RSSReaderTests
+//
+//  Created by Heorhi Heilik on 23.11.23.
+//
+
+import Foundation
+import XCTest
+
+class FeedEntriesTests: XCTestCase {
+
+}
---
+++ RSSReaderTests/FeedSources/MOC/MOCRSSFeed.swift
@@ -0,0 +1,34 @@
+//
+//  MOCRSSFeed.swift
+//  RSSReaderTests
+//
+//  Created by Heorhi Heilik on 23.11.23.
+//
+
+import FeedKit
+import Foundation
+
+extension RSSFeed {
+
+    static let moc: RSSFeed = {
+        let feed = RSSFeed()
+
+        feed.title = "Test"
+        feed.description = "MOC feed for testing."
+
+        let item1 = RSSFeedItem()
+        item1.title = "First Title"
+        item1.description = "First description."
+        item1.pubDate = Date(timeIntervalSince1970: 1078437600)
+
+        let item2 = RSSFeedItem()
+        item2.title = "Second Title"
+        item2.description = "Second description."
+        item2.pubDate = Date(timeIntervalSinceNow: 1693515600)
+
+        feed.items = [item1, item2]
+
+        return feed
+    }()
+
+}
