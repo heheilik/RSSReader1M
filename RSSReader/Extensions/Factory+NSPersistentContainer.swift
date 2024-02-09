e8ef9484b7be143fed234b8721417318306a15d4
Heorhi Heilik <heheilik@icloud.com>
2024-02-09 12:37:14 +0300

    added persistent container builder

--- RSSReader/Extensions/Factory+NSPersistentContainer.swift
+++ RSSReader/Extensions/Factory+NSPersistentContainer.swift
@@ -9,57 +9,13 @@ import CoreData
 import Factory
 import Foundation
 
-private enum ModelNames {
-    static let feed = "FeedModel"
-}
-
 extension Container {
     var feedModelPersistentContainer: Factory<NSPersistentContainer> {
         self {
-            let persistentContainer = NSPersistentContainer(name: ModelNames.feed)
-
-            // getting path to database
-            guard
-                let applicationSupportURL = try? FileManager.default.url(
-                    for: .applicationSupportDirectory,
-                    in: .userDomainMask,
-                    appropriateFor: nil,
-                    create: true
-                ),
-                let databaseURL = URL(
-                    string: "\(ModelNames.feed).sqlite",
-                    relativeTo: applicationSupportURL
-                )
-            else {
-                fatalError("Failed to get path to database.")
+            guard let container = PersistentContainerBuilder.shared.newPersistentContainer(for: .feed) else {
+                fatalError("Failed to load persistent store.")
             }
-
-            // creating description
-            let description = NSPersistentStoreDescription(url: databaseURL)
-            description.setOption(true as NSObject, forKey: NSMigratePersistentStoresAutomaticallyOption)
-            description.setOption(true as NSObject, forKey: NSInferMappingModelAutomaticallyOption)
-
-            // adding store by path and options
-            persistentContainer.persistentStoreCoordinator.addPersistentStore(
-                with: description
-            ) { description, error in
-                if let error {
-                    fatalError("Unable to load persistent stores: \(error)")
-                }
-            }
-
-            // testing new model
-            do {
-                let model = try NSMappingModel.inferredMappingModel(
-                    forSourceModel: persistentContainer.managedObjectModel,
-                    destinationModel: persistentContainer.managedObjectModel
-                )
-                print(model)
-            } catch {
-                print(error)
-            }
-
-            return persistentContainer
+            return container
         }.singleton
     }
 }
