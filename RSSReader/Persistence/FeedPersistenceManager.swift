02d06585436682e6a91cd470d8435ac9c3854560
Heorhi Heilik <heheilik@icloud.com>
2024-02-13 12:09:14 +0300

    CoreData related methods moved to FeedPersistenceManager

--- RSSReader/Persistence/FeedPersistenceManager.swift
+++ RSSReader/Persistence/FeedPersistenceManager.swift
@@ -7,6 +7,7 @@
 
 import CoreData
 import Factory
+import FeedKit
 import Foundation
 
 class FeedPersistenceManager {
@@ -74,14 +75,153 @@ class FeedPersistenceManager {
 
     // MARK: Internal methods
 
-    func changeActiveURL(to url: URL) {
-        fetchedResultsController.fetchRequest.predicate = Self.newPredicateTemplate().withSubstitutionVariables([
-            SubstitutableVariables.url: url
-        ])
-        try? fetchedResultsController.performFetch()
+    @MainActor
+    func fetchControllerData() async -> Bool {
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            return false
+        }
+        return true
+    }
+
+    @MainActor
+    func saveControllerData() async -> Bool {
+        do {
+            try Self.persistentContainer.viewContext.save()
+        } catch {
+            return false
+        }
+        return true
+    }
+
+    func insert(feed downloadedFeed: RSSFeed, downloadedAt url: URL) async {
+        let context = Self.persistentContainer.newBackgroundContext()
+        context.mergePolicy = NSMergePolicy.rollback
+        
+        context.performAndWait {
+            // fetching feed
+            let feed = fetchFeed(by: url, in: context)
+
+            // updating feed with downloaded data
+            if let feed {
+                merge(downloadedFeed: downloadedFeed, into: feed, in: context)
+            } else {
+                createFeed(from: downloadedFeed, downloadedAt: url, in: context)
+            }
+
+            // saving changes
+            if context.hasChanges {
+                try? context.save()
+            }
+        }
+
+        _ = await saveControllerData()
+
+        // TODO: perform fetch if needed
+    }
+
+    @MainActor
+    func fetchUnreadEntriesCount(for url: URL) async -> Int? {
+        // creating expression
+        let countExpression = NSExpression(
+            forFunction: "count:",
+            arguments: [NSExpression(forKeyPath: #keyPath(ManagedFeedEntry.isRead))]
+        )
+
+        // creating expression description
+        let key = "count"
+        let expressionDescription = NSExpressionDescription()
+        expressionDescription.name = key
+        expressionDescription.expression = countExpression
+        if #available(iOS 15, *) {
+            expressionDescription.resultType = .integer64
+        } else {
+            expressionDescription.expressionResultType = .integer64AttributeType
+        }
+
+        // creating fetch predicate
+        let predicate = NSPredicate(
+            format: "%K == %@ AND %K == %@",
+            argumentArray: [
+                #keyPath(ManagedFeedEntry.feed.url),
+                url,
+                #keyPath(ManagedFeedEntry.isRead),
+                false
+            ]
+        )
+
+        // creating fetch request
+        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = ManagedFeedEntry.fetchRequest()
+        fetchRequest.predicate = predicate
+        fetchRequest.propertiesToFetch = [expressionDescription]
+        fetchRequest.resultType = .dictionaryResultType
+
+        // running fetch and acquiring result
+        do {
+            let result = try fetchedResultsController.managedObjectContext.fetch(fetchRequest)
+            guard let int64Result = (result as? [[String: Int64]])?.first?[key] else {
+                return nil
+            }
+            return Int(int64Result)
+        } catch {
+            print(error)
+            return nil
+        }
     }
 
     // MARK: Private methods
+    
+    /// Must be called on private queue of context.
+    private func merge(
+        downloadedFeed: RSSFeed,
+        into feed: ManagedFeed,
+        in context: NSManagedObjectContext
+    ) {
+        guard let entries = feed.entries as? Set<ManagedFeedEntry> else {
+            return
+        }
+
+        // removing entries that are already contained by database
+        downloadedFeed.items = downloadedFeed.items?.filter { item in
+            !entries.contains { $0.title == item.title }
+        }
+
+        // creating new entries in context
+        downloadedFeed.items?.forEach {
+            let managedFeedEntry = ManagedFeedEntry(context: context)
+            guard managedFeedEntry.fill(with: $0) else {
+                context.delete(managedFeedEntry)
+                return
+            }
+            feed.addToEntries(managedFeedEntry)
+        }
+    }
+    
+    /// Must be called on private queue of context.
+    private func createFeed(
+        from downloadedFeed: RSSFeed,
+        downloadedAt url: URL,
+        in context: NSManagedObjectContext
+    ) {
+        let feed = ManagedFeed(context: context)
+        guard feed.fill(with: downloadedFeed, url: url) else {
+            context.delete(feed)
+            return
+        }
+    }
+
+    private func fetchFeed(by url: URL, in context: NSManagedObjectContext) -> ManagedFeed? {
+        let fetchRequest = ManagedFeed.fetchRequest()
+        fetchRequest.predicate = NSPredicate(
+            format: "%K == %@",
+            argumentArray: [
+                #keyPath(ManagedFeed.url),
+                url
+            ]
+        )
+        return try? context.fetch(fetchRequest).first
+    }
 
     private static func newPredicateTemplate() -> NSPredicate {
         NSPredicate(format: "\(#keyPath(ManagedFeedEntry.feed.url)) == $\(SubstitutableVariables.url)")
